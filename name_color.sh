3acd8b49e766a3dfb4e2fe71f074096d23a5d5c3
kirwa-KO <56740152+kirwa-KO@users.noreply.github.com>
2020-02-19 22:41:28 +0100

    add name function

---
+++ name_color.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+time=0.4
+
+i=1
+while $T
+do
+    rand=$[RANDOM % 10 + 30]
+    case $[i % 6] in
+        0) text=${1}. ;;
+        1) text=${1}.. ;;
+        2) text=${1}... ;;
+        3) text=${1}.... ;;
+        4) text=${1}..... ;;
+        5) text=${1}.....! ;;
+    esac
+    figlet ${text} > pattern
+    while IFS= read -r line
+    do
+       echo -e "\033[1;${rand}m${line}\033[0m" 
+    done < pattern
+    sleep $time
+    i=$[i + 1]
+done
+
+
+
+
+
+
+
+
+#!/bin/bash
+
+# time=0.2
+
+# while $T
+# do
+#     rand=$[RANDOM % 10 + 30]
+#     figlet  $1 > pattern
+#     while IFS= read -r line
+#     do
+#        echo -e "\033[1;${rand}m${line}\033[0m" 
+#     done < pattern
+#     sleep 0.2
+#     rand=$[RANDOM % 10 + 30]
+#     figlet  ${1}. > pattern
+#     while IFS= read -r line
+#     do
+#        echo -e "\033[1;${rand}m${line}\033[0m" 
+#     done < pattern
+#     sleep 0.2
+#     rand=$[RANDOM % 10 + 30]
+#     figlet  ${1}.. > pattern
+#     while IFS= read -r line
+#     do
+#        echo -e "\033[1;${rand}m${line}\033[0m" 
+#     done < pattern
+#     sleep 0.2
+#     figlet  ${1}... > pattern
+#     while IFS= read -r line
+#     do
+#        echo -e "\033[1;${rand}m${line}\033[0m" 
+#     done < pattern
+#     sleep 0.2
+#     figlet  ${1}.... > pattern
+#     while IFS= read -r line
+#     do
+#        echo -e "\033[1;${rand}m${line}\033[0m" 
+#     done < pattern
+#     sleep 0.2
+#     figlet  ${1}....! > pattern
+#     while IFS= read -r line
+#     do
+#        echo -e "\033[1;${rand}m${line}\033[0m" 
+#     done < pattern
+#     sleep 0.2
+# done
