3a1a51a9e89996a6ab9f6aef7146721b8c201aae
kirwa-KO <56740152+kirwa-KO@users.noreply.github.com>
2020-02-23 12:01:40 +0100

    add parser test

---
+++ testParser.sh
@@ -0,0 +1,38 @@
+echo thala teste gog igii
+echo "test ghnfrhjbfrjfbvkdfbvkhbdfkvhbfdkjvbdfjhbvdf                   |;dsvhbfvjhbfsjvbfjsbvbfs"
+echo \"$PATH | ls | echo $HOME | echo $THALA
+echo
+echo -n
+echo echo echo echo echo ls
+echo '$PATH'$PATH"$HMAD""$HOME"
+echo "\$PATH"
+echo '\$PATH'
+echo '$PATH'
+echo ""
+echo "test"$HOME'gogo'
+echo testd testd thala testd go HOME
+echo \"$PATH'test'
+echo \ \ \ \ \ \ \ \  \ \ \ \ \ \ \ \ \ \ \      | ls
+echo '"$PATH"'
+echo {$PATH} | cat -e
+echo {\$PATH} > test
+echo <>
+echo >
+echo <
+echo >>
+echo >> d > f>d>d>D>D>D>D>D>>ls
+echo thala;ls>1>2>3>4>5>5>45>42>15>4121>24>51>25>42>1
+echo \|\"$PATH
+export x=5
+echo The value of variable x = $x 
+echo -n "Tecmint \"bis \ba \"bcommunity \"bof \$bLinux \|bNerds"
+echo -n "Tecmint \nis \na \ncommunity \nof \nLinux \nNerds"
+"echo" $PATH
+export y=ho
+ec$y $HOME
+export p=r
+c'l'e"a"$p
+echo "I'm a Linux user."
+echo $?
+echo $hh@lo
+echo "$HOME\flkgjdlkfgj"
