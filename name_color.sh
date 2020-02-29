22e72f6160ee3aac9ccd811ce80432bf197c9b01
kirwa-KO <56740152+kirwa-KO@users.noreply.github.com>
2020-02-29 10:49:55 +0100

    Update name_color.sh

--- name_color.sh
+++ name_color.sh
@@ -2,25 +2,23 @@
 
 time=0.4
 
-
+i=1
 while $T
 do
     rand=$[RANDOM % 10 + 30]
     case $[i % 6] in
-        0) text=${var}. ;;
-        1) text=${var}.. ;;
-        2) text=${var}... ;;
-        3) text=${var}.... ;;
-        4) text=${var}..... ;;
-        5) text=${var}.....! ;;
+        0) text=${1}. ;;
+        1) text=${1}.. ;;
+        2) text=${1}... ;;
+        3) text=${1}.... ;;
+        4) text=${1}..... ;;
+        5) text=${1}.....! ;;
     esac
-    figlet -f basic ${text} > pattern
+    figlet ${text} > pattern
     while IFS= read -r line
     do
        echo -e "\033[1;${rand}m${line}\033[0m" 
     done < pattern
-    rm -rf pattern
     sleep $time
     i=$[i + 1]
-
 done
